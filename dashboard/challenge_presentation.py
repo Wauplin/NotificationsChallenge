3d870109dc92912debc29b0a8602563b6871926b
Lucain Pouget <lucain.pouget@implicity.fr>
2020-02-06 23:53:06 +0100

    Add readme to challenge presentation

--- dashboard/challenge_presentation.py
+++ dashboard/challenge_presentation.py
@@ -3,4 +3,7 @@ from dashboard.st_utils.write_md import write_md
 
 
 def challenge_presentation():
+    st.title('Summary')
     write_md('doc/summary.md')
+    st.title('Readme')
+    write_md('README.md')
