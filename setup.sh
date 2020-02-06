8d41f08d262f1868c6f3eadd74f22979a0b422c1
Lucain Pouget <lucain.pouget@implicity.fr>
2020-02-06 10:41:04 +0100

    Heroku app

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# https://gilberttanner.com/blog/deploying-your-streamlit-dashboard-with-heroku
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
